ff1545873d1aeadc06b77d00baf2c014f6992619
Joan Martinez <joan.fontanals.martinez@jina.ai>
2024-04-10 16:09:51 +0200

    feat: compute for multi models

--- scripts/run_mteb_quantization.py
+++ scripts/run_mteb_quantization.py
@@ -13,22 +13,21 @@ logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger("main")
 
-
 TASK_LIST_RETRIEVAL = [
     "ArguAna",
     # "ClimateFEVER",
-    "CQADupstackAndroidRetrieval",
-    "CQADupstackEnglishRetrieval",
-    "CQADupstackGamingRetrieval",
-    "CQADupstackGisRetrieval",
-    "CQADupstackMathematicaRetrieval",
-    "CQADupstackPhysicsRetrieval",
-    "CQADupstackProgrammersRetrieval",
-    "CQADupstackStatsRetrieval",
-    "CQADupstackTexRetrieval",
-    "CQADupstackUnixRetrieval",
-    "CQADupstackWebmastersRetrieval",
-    "CQADupstackWordpressRetrieval",
+    # "CQADupstackAndroidRetrieval",
+    # "CQADupstackEnglishRetrieval",
+    # "CQADupstackGamingRetrieval",
+    # "CQADupstackGisRetrieval",
+    # "CQADupstackMathematicaRetrieval",
+    # "CQADupstackPhysicsRetrieval",
+    # "CQADupstackProgrammersRetrieval",
+    # "CQADupstackStatsRetrieval",
+    # "CQADupstackTexRetrieval",
+    # "CQADupstackUnixRetrieval",
+    # "CQADupstackWebmastersRetrieval",
+    # "CQADupstackWordpressRetrieval",
     "DBPedia",
     # "FEVER",
     "FiQA2018",
@@ -47,36 +46,45 @@ QUANTIZATION = ['ubinary', 'int8']
 
 TASK_LIST = TASK_LIST_RETRIEVAL
 
-model_name = "jinaai/jina-embeddings-v2-base-en"
-model = DRESModel(SentenceTransformer(model_name, trust_remote_code=True, device='cuda:0'))
-
-old_model_encode_queries = model.encode_queries
-old_model_encode_corpus = model.encode_corpus
+model_name_en = "jinaai/jina-embeddings-v2-base-en"
+model_name_de = "jinaai/jina-embeddings-v2-base-de"
+model_name_es = "jinaai/jina-embeddings-v2-base-es"
+model_name_zh = "jinaai/jina-embeddings-v2-base-zh"
+model_name_code = "jinaai/jina-embeddings-v2-base-code"
 
 batch_size_map = {'CQADupstackAndroidRetrieval': 1,
                   'CQADupstackEnglishRetrieval': 1,
                   'CQADupstackGamingRetrieval': 1,
                   'CQADupstackGisRetrieval': 1, 'CQADupstackMathematicaRetrieval': 1, 'CQADupstackPhysicsRetrieval': 1,
                   'CQADupstackProgrammersRetrieval': 1, 'CQADupstackStatsRetrieval': 1, 'CQADupstackTexRetrieval': 1,
-                  'CQADupstackUnixRetrieval': 1, 'CQADupstackWebmastersRetrieval': 1, 'CQADupstackWordpressRetrieval': 1}
+                  'CQADupstackUnixRetrieval': 1, 'CQADupstackWebmastersRetrieval': 1,
+                  'CQADupstackWordpressRetrieval': 1}
+
 QUANT_FUNTION = {'ubinary': 'dot', 'int8': 'cos_sim'}
 
-for task in TASK_LIST:
-    for quant in QUANTIZATION:
-        logger.info(f"Running task: {task} with quantization: {quant}")
-        # normalize_embeddings should be true for this model
-        if quant == 'int8':
-            model.encode_queries = functools.partial(old_model_encode_queries, precision=quant)
-            model.encode_corpus = functools.partial(old_model_encode_corpus, precision=quant)
-        else:
-            model.encode_queries = functools.partial(old_model_encode_queries, precision='float32')
-            model.encode_corpus = functools.partial(old_model_encode_corpus, precision=quant)
-        eval_splits = ["dev"] if task == "MSMARCO" else ["test"]
-        evaluation = MTEB(
-            tasks=[task], task_langs=["en"]
-        )  # Remove "en" for running all languages
-        evaluation.run(
-            model, output_folder=f"results/{quant}/{model_name}", eval_splits=eval_splits,
-            score_function=QUANT_FUNTION[quant], batch_size=batch_size_map.get(task, 1), convert_to_tensor=False
-        )
+models = [model_name_en, model_name_de, model_name_es, model_name_zh, model_name_code]
 
+for model_name in models:
+    model = DRESModel(SentenceTransformer(model_name, trust_remote_code=True, device='cuda:0'))
+
+    old_model_encode_queries = model.encode_queries
+    old_model_encode_corpus = model.encode_corpus
+
+    for task in TASK_LIST:
+        for quant in QUANTIZATION:
+            logger.info(f"Running task: {task} with quantization: {quant}")
+            # normalize_embeddings should be true for this model
+            if quant == 'int8':
+                model.encode_queries = functools.partial(old_model_encode_queries, precision=quant)
+                model.encode_corpus = functools.partial(old_model_encode_corpus, precision=quant)
+            else:
+                model.encode_queries = functools.partial(old_model_encode_queries, precision='float32')
+                model.encode_corpus = functools.partial(old_model_encode_corpus, precision=quant)
+            eval_splits = ["dev"] if task == "MSMARCO" else ["test"]
+            evaluation = MTEB(
+                tasks=[task], task_langs=["en"]
+            )  # Remove "en" for running all languages
+            evaluation.run(
+                model, output_folder=f"results/{quant}/{model_name}", eval_splits=eval_splits,
+                score_function=QUANT_FUNTION[quant], batch_size=batch_size_map.get(task, 1), convert_to_tensor=False
+            )
